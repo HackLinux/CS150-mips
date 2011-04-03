b2b821f1ebff02bf12cb9f61a8a4b5a903849ed0
Rishi Chopra <cs150-bd@eecs.berkeley.edu>
2011-04-03 01:10:21 -0700

    r val e

--- hardware/src/controller.v
+++ hardware/src/controller.v
@@ -1,27 +1,30 @@
 module controller(	input  [5:0]	Op, Funct,
                   	input        	ZeroE,
-			output		JALValE, JALDstE, 
+			output		JALValM, JALDstM, 
 			output [1:0]	PCBranchAddrE,
-			output		RegWriteE, 
+			output		RegWriteM, 
 			output		SignOrZeroE,
-			output		RegDstE, RegValE,
+			output		RegDstE,
 			output [1:0]	ALUSrcE, 
-			output		MemToRegE,
+			output		MemToRegM,
 			output [3:0]	ALUControlE,
 			output [1:0]	MaskControlE,
-			output [1:0]	LBLHEnableE,
-			output       	JumpE);
+			output [1:0]	LBLHEnableM
+			output       	JumpM);
 
 	wire [1:0] aluop;
 	wire [3:0] maskop;
 
-//assign {JumpE, JALValE, JALDstE, RegWriteE, SignOrZeroE, RegDstE, ALUSrcE, RegValE, PCBranchAddrE, MemToRegE, ALUop, MaskOp} = controls;
+assign {JumpM, JALValM, JALDstM, RegWriteM, 
+	SignOrZeroE, RegDstE, ALUSrcE, 
+	RegValE, PCBranchAddrE, MemToRegM, 
+	ALUop, MaskOp} = controls;
 
 
-	maindec mnd(Op, Funct, JALValE, JALDstE, PCBranchAddrE, RegWriteE, SignOrZeroE, RegDstE, RegValE, ALUSrcE, MemToRegE, ALUOp, MaskOp);
-	aludec  ad(Funct, Op, ALUOp, ALUControlE);
-	maskdec mkd(MaskOp, MaskControlE, LBLHEnableE);
+	maindec mnd(Op, Funct, JALValM, JALDstM, PCBranchAddrE, RegWriteM, SignOrZeroE, RegDstE, ALUSrcE, MemToRegM, ALUOp, MaskOp);
+	aludec  ad(Funct, ALUOp, ALUControlE);
+	maskdec mkd(MaskOp, MaskControlE, LBLHEnableM);
 
-  	assign JumpE = ALUControlE[3] & ZeroE;
+  	assign JumpM = ALUControlE[3] & ZeroE;
 
 endmodule
--- hardware/src/maindec.v
+++ hardware/src/maindec.v
@@ -1,56 +1,56 @@
-module maindec(	input [5:0] 	op,
+module maindec(	input [5:0] 	Op,
 		input [5:0] 	Funct,
 		output		JALValM, JALDstM, 
 		output [1:0]	PCBranchAddrE,
 		output		RegWriteM, 
 		output		SignOrZeroE,
-		output		RegDstE, RegValE,
+		output		RegDstE,
 		output [1:0]	ALUSrcE, 
 		output		MemWriteM,
 		output		MemToRegM,
 		output [1:0]	ALUOp,
 		output [3:0]	MaskOp);
 
-reg [17:0] controls;
+reg [8:0] controls;
 
 assign {JumpM, JALValM, JALDstM, RegWriteM, 
 	SignOrZeroE, RegDstE, ALUSrcE, 
-	RegValE, PCBranchAddrE, MemToRegM, 
+	PCBranchAddrE, MemToRegM, 
 	ALUop, MaskOp} = controls;
 
 //assume SignOrZeroE == 0 is zero-extend
 
 always @ (*)
 	case(op)
-		6'b000001: controls <= 18'b1XXX1X00X010100000; //BLTZ,BGEZ
-		6'b000010: controls <= 18'b1XXXXXXXX11XXX0000; //J
-		6'b000011: controls <= 18'b111XXXXXX11XXX0000; //JAL
-		6'b000100: controls <= 18'b1XXX1X00X010100000; //BEQ
-		6'b000101: controls <= 18'b1XXX1X00X010100000; //BNE
-		6'b000110: controls <= 18'b1XXX1X00X010100000; //BLEZ
-		6'b000111: controls <= 18'b1XXX1X00X010100000; //BGTZ
-		6'b001001: controls <= 18'b000110010XX0000000; //ADDIU
-		6'b001010: controls <= 18'b000110010XX0110000; //SLTI
-		6'b001011: controls <= 18'b000110010XX0110000; //SLTIU
-		6'b001100: controls <= 18'b000100010XX0110000; //ANDI
-		6'b001101: controls <= 18'b000100010XX0110000; //ORI
-		6'b001110: controls <= 18'b000100010XX0110000; //XORI
-		6'b001111: controls <= 18'b0001X0100XX0110000; //LUI
-		6'b100000: controls <= 18'b00011X01XXX1000001; //LB
-		6'b100001: controls <= 18'b00011X01XXX1000010; //LH
-		6'b100011: controls <= 18'b00011X01XXX1000011; //LW
-		6'b100100: controls <= 18'b00011X01XXX1000100; //LBU
-		6'b100101: controls <= 18'b00011X01XXX1000101; //LHU
-		6'b101000: controls <= 18'b00000X01XXX0000110; //SB
-		6'b101001: controls <= 18'b00000X01XXX0000111; //SH
-		6'b101011: controls <= 18'b00000X01XXX0001000; //SW
+		6'b000001: controls <= 17'b1XXX1X00010100000; //BLTZ,BGEZ
+		6'b000010: controls <= 17'b1XXXXXXX11XXX0000; //J
+		6'b000011: controls <= 17'b111XXXXX11XXX0000; //JAL
+		6'b000100: controls <= 17'b1XXX1X00010100000; //BEQ
+		6'b000101: controls <= 17'b1XXX1X00010100000; //BNE
+		6'b000110: controls <= 17'b1XXX1X00010100000; //BLEZ
+		6'b000111: controls <= 17'b1XXX1X00010100000; //BGTZ
+		6'b001001: controls <= 17'b00011001XX0000000; //ADDIU
+		6'b001010: controls <= 17'b00011001XX0110000; //SLTI
+		6'b001011: controls <= 17'b00011001XX0110000; //SLTIU
+		6'b001100: controls <= 17'b00010001XX0110000; //ANDI
+		6'b001101: controls <= 17'b00010001XX0110000; //ORI
+		6'b001110: controls <= 17'b00010001XX0110000; //XORI
+		6'b001111: controls <= 17'b0001X010XX0110000; //LUI
+		6'b100000: controls <= 17'b00011X01XX1000001; //LB
+		6'b100001: controls <= 17'b00011X01XX1000010; //LH
+		6'b100011: controls <= 17'b00011X01XX1000011; //LW
+		6'b100100: controls <= 17'b00011X01XX1000100; //LBU
+		6'b100101: controls <= 17'b00011X01XX1000101; //LHU
+		6'b101000: controls <= 17'b00000X01XX0000110; //SB
+		6'b101001: controls <= 17'b00000X01XX0000111; //SH
+		6'b101011: controls <= 17'b00000X01XX0001000; //SW
 		default:   case(Funct) 
-			6'b000000: controls <= 18'b000111110XX0110000; //SLL
-			6'b000010: controls <= 18'b000111110XX0110000; //SRL
-			6'b000011: controls <= 18'b000111110XX0110000; //SRA
-			6'b001000: controls <= 18'b1XXXXXXXX00XXX0000; //JR
-			6'b001001: controls <= 18'b110XXXXX100XXX0000; //JALR
-			default:   controls <= 18'b000111000XX0110000; //All Other R-type
+			6'b000000: controls <= 17'b00011111XX0110000; //SLL
+			6'b000010: controls <= 17'b00011111XX0110000; //SRL
+			6'b000011: controls <= 17'b00011111XX0110000; //SRA
+			6'b001000: controls <= 17'b1XXXXXXX00XXX0000; //JR
+//			6'b001001: controls <= 17'b110XXXXX00XXX0000; //JALR
+			default:   controls <= 17'b00011100XX0110000; //All Other R-type
 		endcase
 	endcase
 endmodule
